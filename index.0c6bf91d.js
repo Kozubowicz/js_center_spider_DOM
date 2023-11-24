9fb8b370e6d57efdf7ef5617d14eb419aaa223fe
Kozubowicz <55114447+Kozubowicz@users.noreply.github.com>
2023-11-24 23:00:09 +0100

    make build

---
+++ index.0c6bf91d.js
@@ -0,0 +1,2 @@
+var e=document.querySelector(".wall"),t=document.querySelector(".spider"),l=(e.scrollWidth-t.width)/2,r=(e.scrollHeight-t.height)/2;t.style.marginTop=r+"px",t.style.marginLeft=l+"px";
+//# sourceMappingURL=index.0c6bf91d.js.map
